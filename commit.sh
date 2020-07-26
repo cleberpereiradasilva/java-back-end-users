faf71ecf3d23721516c8dacefbbb441b4bcce1c3
Cleber Silva <cleber_pesilva@yahoo.com.br>
2020-07-26 02:08:06 -0300

    Created UserController and scripts to commit and run

--- commit.sh
+++ commit.sh
@@ -2,8 +2,8 @@
 commit_message="$1"
 git add . -A
 branch=$(git branch 2>/dev/null | grep '^*' | colrm 1 2 | awk '{if ($1 !="") print $1}')
-echo "Message in $commit_message"
-echo "Branch $branch"
+echo "Message: $commit_message"
+echo "Branch: $branch"
 
 git commit -m "$commit_message"
 git push origin "$branch"
