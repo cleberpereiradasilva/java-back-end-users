c30a37b1ac579040e223f8f3e8ddfb2899f3cb54
Cleber Silva <cleber_pesilva@yahoo.com.br>
2020-07-26 02:06:03 -0300

    Created UserController and scripts to commit and run

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 commit_message="$1"
 git add . -A
-branch=$(git branch 2>/dev/null | grep '^*' | colrm 1 2 | awk '{if ($1 !="") print "("$1")"}')
+branch=$(git branch 2>/dev/null | grep '^*' | colrm 1 2 | awk '{if ($1 !="") print $1}')
 git commit -m "$commit_message"
 git push "$branch"
