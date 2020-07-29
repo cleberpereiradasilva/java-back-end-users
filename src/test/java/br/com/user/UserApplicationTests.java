61c594684541fd44e5ddb89e1b9c86f29ed3944b
Cleber Silva <cleber_pesilva@yahoo.com.br>
2020-07-29 07:21:01 -0300

    First test, only validate '/user'

--- src/test/java/br/com/user/UserApplicationTests.java
+++ src/test/java/br/com/user/UserApplicationTests.java
@@ -5,26 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class UserApplicationTests {
-
-    @Autowired
-    private MockMvc mocMvc;
-
-
 	@Test
 	void contextLoads() {
 	}
-
-  @Test
-  void liseUserTest() throws Exception{
-   this.mocMvc
-       .perform(get("/user"))
-       .andExpect(status().isOk());
-  }
 
 }
