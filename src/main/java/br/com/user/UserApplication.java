d66bc4e71f2d5c84e09f2e9eb568009e2001ac32
Cleber Silva <cleber_pesilva@yahoo.com.br>
2020-08-02 21:43:34 -0300

    feat:update swagger to v3

--- src/main/java/br/com/user/UserApplication.java
+++ src/main/java/br/com/user/UserApplication.java
@@ -2,8 +2,12 @@ package br.com.user;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
 public class UserApplication {
